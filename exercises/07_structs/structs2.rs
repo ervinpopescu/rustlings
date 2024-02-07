38a31b87884bbac13983531991302ee6386a13cd
ervinpopescu <ervin.popescu10@gmail.com>
2024-02-07 09:44:47 +0200

    Initial commit for solutions branch

--- exercises/07_structs/structs2.rs
+++ exercises/07_structs/structs2.rs
@@ -5,8 +5,6 @@
 // Execute `rustlings hint structs2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
 #[derive(Debug)]
 struct Order {
     name: String,
@@ -32,13 +30,23 @@ fn create_order_template() -> Order {
 
 #[cfg(test)]
 mod tests {
+    use std::fmt::Pointer;
+
     use super::*;
 
     #[test]
     fn your_order() {
         let order_template = create_order_template();
         // TODO: Create your own order using the update syntax and template above!
-        // let your_order =
+        let your_order = Order {
+            name: "Hacker in Rust".to_string(),
+            year: 2019,
+            made_by_phone: false,
+            made_by_mobile: false,
+            made_by_email: true,
+            item_number: 123,
+            count: 1,
+        };
         assert_eq!(your_order.name, "Hacker in Rust");
         assert_eq!(your_order.year, order_template.year);
         assert_eq!(your_order.made_by_phone, order_template.made_by_phone);
